52c29ba342c56fe7ddf7b62cfe4a770aac9d8e1d
Aaron Schmid <as.sch@gmx.ch>
2017-03-29 10:15:56 +0200

    Updated Error Information list

--- app/lib/core.js
+++ app/lib/core.js
@@ -10,7 +10,7 @@ module.exports.getInformationForError = function(error) {
 	switch(error) {
 		case 'test': return ['This is a Test Error', 'Ignore me!'];
 		case 'invalid_grant': return ['Retry Authentication with your Google Account']
-		case 'no_auth': return ['Retry Authentication with your Google Account']
+		case 'no_auth': return ['Retry Authentication with your Google Account', 'Remove permission for this Application on your Google Account']
 		case 'no_refresh_token': return ['Retry Authentication with your Google Account']
 	}
 }
